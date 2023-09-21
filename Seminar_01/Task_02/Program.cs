13c805f15b67f882709658759a15f3f68d23562c
Rudik2012 <rudik2012@gmail.com>
2023-09-21 21:53:51 +0700

    доработал Task_2 еще

--- Seminar_01/Task_02/Program.cs
+++ Seminar_01/Task_02/Program.cs
@@ -5,27 +5,27 @@ if (a == 1)
 {
 Console.WriteLine("Понедельник");
 }
-if (a == 2)
+else if (a == 2)
 {
 Console.WriteLine("Вторник");
 }
-if (a == 3)
+else if (a == 3)
 {
 Console.WriteLine("Среда");
 }
-if (a == 4)
+else if (a == 4)
 {
 Console.WriteLine("Четверг");
 }
-if (a == 5)
+else if (a == 5)
 {
 Console.WriteLine("Пятница");
 }
-if (a == 6)
+else if (a == 6)
 {
 Console.WriteLine("Суббота");
 }
-if (a == 7)
+else if (a == 7)
 {
 Console.WriteLine("Воскресенье");
 }
